b26426408f95d0382091bba5d0de03cbb08cc3cc
jovany prophete <jovanyprophete23@outlook.com>
2026-08-16 04:52:57 -0500

    Remove all discord.ext.menus dependencies and use custom implementations

--- utils/paginators.py
+++ utils/paginators.py
@@ -4,111 +4,92 @@ import discord
 from utils.config import BotName
 from discord.ext import commands
 
-from .paginator import Paginator as EmbedPaginator
+from .paginator import Paginator as EmbedPaginator, PageSource, ListPageSource
 from discord.ext.commands import Context, Paginator as CmdPaginator
 from typing import Any, List
 
-# discord.ext.menus is deprecated, using modern pagination
-try:
-    from discord.ext import menus
-    MENUS_AVAILABLE = True
-except ImportError:
-    menus = None
-    MENUS_AVAILABLE = False
+
+class FieldPagePaginator(ListPageSource):
+
+    def __init__(self,
+                 entries: list[tuple[Any, Any]],
+                 *,
+                 per_page: int = 10,
+                 inline: bool = False,
+                 **kwargs) -> None:
+        super().__init__(entries, per_page=per_page)
+        self.embed: discord.Embed = discord.Embed(
+            title=kwargs.get('title'),
+            description=kwargs.get('description'),
+            color=0xFF0000)
+        self.inline: bool = inline
+        self.kwargs = kwargs
+
+    async def format_page(self, menu: EmbedPaginator,
+                          entries: list[tuple[Any, Any]]) -> discord.Embed:
+        self.embed.clear_fields()
+
+        for key, value in entries:
+            self.embed.add_field(name=key, value=value, inline=self.inline)
+
+        maximum = self.get_max_pages()
+        if maximum > 1:
+            text = f'• Page {menu.current_page + 1}/{maximum} | Requested by {menu.ctx.author.display_name}'
+            self.embed.set_footer(
+                text=text,
+                icon_url=
+                "https://cdn.discordapp.com/icons/699587669059174461/f689b4366447d5a23eda8d0ec749c1ba.png?width=115&height=115"
+            )
+            #self.embed.timestamp = discord.utils.utcnow()
+        return self.embed
 
 
-if MENUS_AVAILABLE:
-    class FieldPagePaginator(menus.ListPageSource):
+class TextPaginator(ListPageSource):
 
-        def __init__(self,
-                     entries: list[tuple[Any, Any]],
-                     *,
-                     per_page: int = 10,
-                     inline: bool = False,
-                     **kwargs) -> None:
-            super().__init__(entries, per_page=per_page)
-            self.embed: discord.Embed = discord.Embed(
-                title=kwargs.get('title'),
-                description=kwargs.get('description'),
-                color=0xFF0000)
-            self.inline: bool = inline
+    def __init__(self, text, *, prefix='```', suffix='```', max_size=2000):
+        pages = CmdPaginator(prefix=prefix,
+                             suffix=suffix,
+                             max_size=max_size - 200)
+        for line in text.split('\n'):
+            pages.add_line(line)
 
-        async def format_page(self, menu: EmbedPaginator,
-                              entries: list[tuple[Any, Any]]) -> discord.Embed:
-            self.embed.clear_fields()
+        super().__init__(pages.pages, per_page=1)
+        self.BotName = BotName
 
-            for key, value in entries:
-                self.embed.add_field(name=key, value=value, inline=self.inline)
-
-            maximum = self.get_max_pages()
-            if maximum > 1:
-                text = f'• Page {menu.current_page + 1}/{maximum} | Requested by {menu.ctx.author.display_name}'
-                self.embed.set_footer(
-                    text=text,
-                    icon_url=
-                    "https://cdn.discordapp.com/icons/699587669059174461/f689b4366447d5a23eda8d0ec749c1ba.png?width=115&height=115"
-                )
-                #self.embed.timestamp = discord.utils.utcnow()
-            return self.embed
+    async def format_page(self, menu, content):
+        maximum = self.get_max_pages()
+        if maximum > 1:
+            return f'{content} {self.BotName} • Page {menu.current_page + 1}/{maximum}'
+        return content
 
 
-    class TextPaginator(menus.ListPageSource):
+class DescriptionEmbedPaginator(ListPageSource):
 
-        def __init__(self, text, *, prefix='```', suffix='```', max_size=2000):
-            pages = CmdPaginator(prefix=prefix,
-                                 suffix=suffix,
-                                 max_size=max_size - 200)
-            for line in text.split('\n'):
-                pages.add_line(line)
+    def __init__(self,
+                 entries: list[Any],
+                 *,
+                 per_page: int = 10,
+                 **kwargs) -> None:
+        super().__init__(entries, per_page=per_page)
+        self.embed: discord.Embed = discord.Embed(
+            title=kwargs.get('title'),
+            color=0xFF0000,
+        )
+        self.kwargs = kwargs
 
-            super().__init__(entries=pages.pages, per_page=1)
+    async def format_page(self, menu: EmbedPaginator,
+                          entries: list[tuple[Any, Any]]) -> discord.Embed:
+        self.embed.clear_fields()
 
-        async def format_page(self, menu, content):
-            maximum = self.get_max_pages()
-            if maximum > 1:
-                return f'{content} {BotName} • Page {menu.current_page + 1}/{maximum}'
-            return content
-
-
-    class DescriptionEmbedPaginator(menus.ListPageSource):
-
-        def __init__(self,
-                     entries: list[Any],
-                     *,
-                     per_page: int = 10,
-                     **kwargs) -> None:
-            super().__init__(entries, per_page=per_page)
-            self.embed: discord.Embed = discord.Embed(
-                title=kwargs.get('title'),
-                color=0xFF0000,
+        self.embed.description = '\n'.join(entries)
+        #self.embed.timestamp = discord.utils.utcnow()
+        maximum = self.get_max_pages()
+        if maximum > 1:
+            text = f'• Page {menu.current_page + 1}/{maximum} | Requested by {menu.ctx.author.display_name}'
+            self.embed.set_footer(
+                text=text,
+                icon_url=
+                "https://cdn.discordapp.com/icons/699587669059174461/f689b4366447d5a23eda8d0ec749c1ba.png?width=115&height=115"
             )
 
-        async def format_page(self, menu: EmbedPaginator,
-                              entries: list[tuple[Any, Any]]) -> discord.Embed:
-            self.embed.clear_fields()
-
-            self.embed.description = '\n'.join(entries)
-            #self.embed.timestamp = discord.utils.utcnow()
-            maximum = self.get_max_pages()
-            if maximum > 1:
-                text = f'• Page {menu.current_page + 1}/{maximum} | Requested by {menu.ctx.author.display_name}'
-                self.embed.set_footer(
-                    text=text,
-                    icon_url=
-                    "https://cdn.discordapp.com/icons/699587669059174461/f689b4366447d5a23eda8d0ec749c1ba.png?width=115&height=115"
-                )
-
-            return self.embed
-else:
-    # Fallback classes when menus is not available
-    class FieldPagePaginator:
-        def __init__(self, *args, **kwargs):
-            raise NotImplementedError("discord.ext.menus is not available. Please use modern Discord UI views.")
-
-    class TextPaginator:
-        def __init__(self, *args, **kwargs):
-            raise NotImplementedError("discord.ext.menus is not available. Please use modern Discord UI views.")
-
-    class DescriptionEmbedPaginator:
-        def __init__(self, *args, **kwargs):
-            raise NotImplementedError("discord.ext.menus is not available. Please use modern Discord UI views.")
+        return self.embed
