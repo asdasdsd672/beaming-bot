b4704b381518bc97cd8f9e67894c3142736d8b78
jovany prophete <jovanyprophete23@outlook.com>
2026-08-16 04:41:18 -0500

    Include config.py in repository for Render deployment

---
+++ utils/config.py
@@ -0,0 +1,9 @@
+import os
+
+TOKEN = os.environ.get("TOKEN")
+NAME = "BeZmerz"
+server = "https://discord.gg/P9UXgfMT6P"
+ch = "https://discord.com/channels/699587669059174461/1271825678710476911"
+OWNER_IDS = [1527881220741009623]
+BotName = "BeZmerz"
+serverLink = "https://discord.gg/P9UXgfMT6P"
